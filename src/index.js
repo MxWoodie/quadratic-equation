bb41fbf01201313f8936505463b292231a2ff0fa
mxwoodie <maxswation@gmail.com>
2018-09-26 16:14:26 +0300

    solveQuadEq

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+    let a = equation.split('*')[0].replace(/\s/g, ''); 
+    let b = equation.split('*')[1].replace('x^2','').replace(/\s/g, ''); 
+    let c = equation.split('*')[2].replace('x','').replace(/\s/g, '');
+
+    let discr = b*b - 4 * a * c;
+
+    let x1 = Math.round((-b + Math.sqrt(discr))/(2*a));
+    let x2 = Math.round((-b - Math.sqrt(discr))/(2*a));
+
+    let solutions = [x1,x2];
+    solutions.sort((a,b)=> {
+        return a - b;
+    });
+    
+    return solutions;
 }
